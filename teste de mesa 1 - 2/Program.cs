9d763af4de71851752111ff8e1348b2f1393d5e9
walterbrito <walterbrito@CQI-BNE-2446>
2025-02-19 17:48:34 -0300

    Adicionar arquivos de projeto.

---
+++ teste de mesa 1 - 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace testeDeMesa2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Objects() 
+    { 
+        
+    }
+}
