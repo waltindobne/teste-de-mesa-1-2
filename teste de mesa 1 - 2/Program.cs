04a6778741c22c4cbf9f90b12df897a973ab3e38
walterbrito <walter.222brito@gmail.com>
2025-02-20 13:41:50 -0300

    primeiro commit

--- teste de mesa 1 - 2/Program.cs
+++ teste de mesa 1 - 2/Program.cs
@@ -6,11 +6,27 @@ namespace testeDeMesa2
     {
         static void Main(string[] args)
         {
-
+            Objects objects = new Objects();
+            objects.Loop();
         }
     }
     public class Objects() 
-    { 
-        
+    {
+        int a = 2;
+        string c = "verdadeiro";
+        int i = 0;
+        public void Loop()
+        {
+            while(a <= 6)
+            {
+                ++a;
+                i++;
+                if(a-1 == 6)
+                {
+                    c = "Falso";
+                }
+                Console.WriteLine($"posição: {i-1}, variavel: {a-1}, vetor[{i-1}]: {a*10}, contador: {a}, condição: {c}");
+            }
+        }
     }
 }
